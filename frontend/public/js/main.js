5ca4da0051e361c49fef7b31cbd2664db71a8a43
Mark Backman <mark@daily.co>
2024-11-17 18:10:02 -0500

    Add merge nodes

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -272,6 +272,50 @@ class PipecatFunctionNode extends LiteGraph.LGraphNode {
   }
 }
 
+class PipecatMergeNode extends LiteGraph.LGraphNode {
+  constructor() {
+    super();
+    this.title = 'Merge';
+
+    // Start with two input slots (minimum)
+    this.addInput('In 1', 'flow');
+    this.addInput('In 2', 'flow');
+
+    this.addOutput('Out', 'flow');
+    this.color = '#95a5a6';
+    this.size = [140, 60]; // Increased height for buttons
+
+    // Add buttons
+    this.addWidget('button', '+ Add input', null, () => {
+      this.addInput(`In ${this.inputs.length + 1}`, 'flow');
+      this.size[1] += 20; // Increase height to accommodate new input
+    });
+
+    this.addWidget('button', '- Remove input', null, () => {
+      if (this.inputs.length > 2) {
+        // Maintain minimum of 2 inputs
+        // Disconnect any existing connection to the last input
+        if (this.inputs[this.inputs.length - 1].link != null) {
+          this.disconnectInput(this.inputs.length - 1);
+        }
+        // Remove the last input
+        this.removeInput(this.inputs.length - 1);
+        this.size[1] -= 20; // Decrease height
+      }
+    });
+  }
+
+  onDrawForeground(ctx) {
+    const activeConnections = this.inputs.filter(
+      (input) => input.link != null
+    ).length;
+    if (activeConnections > 0) {
+      ctx.fillStyle = '#ddd';
+      ctx.font = '11px Arial';
+    }
+  }
+}
+
 // Side panel management
 function updateSidePanel(node) {
   const content = document.querySelector('.editor-content');
@@ -380,6 +424,7 @@ LiteGraph.registerNodeType('nodes/Start', PipecatStartNode);
 LiteGraph.registerNodeType('nodes/Flow', PipecatFlowNode);
 LiteGraph.registerNodeType('nodes/End', PipecatEndNode);
 LiteGraph.registerNodeType('nodes/Function', PipecatFunctionNode);
+LiteGraph.registerNodeType('flow/Merge', PipecatMergeNode);
 
 // Add this function to handle saving
 function generateFlowConfig(graphInstance) {
