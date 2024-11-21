82eb46cb6cd2c23e4077b88ed9c7daa66702c59c
Mark Backman <mark@daily.co>
2024-11-21 00:49:25 -0500

    Add zoom buttons

--- editor/js/editor/canvas.js
+++ editor/js/editor/canvas.js
@@ -12,6 +12,22 @@
 export function setupCanvas(graph) {
   const canvas = new LGraphCanvas("#graph-canvas", graph);
 
+  document.getElementById("zoom-in").onclick = () => {
+    if (canvas.ds.scale < 2) {
+      // Limit max zoom
+      canvas.ds.scale *= 1.2;
+      canvas.setDirty(true, true);
+    }
+  };
+
+  document.getElementById("zoom-out").onclick = () => {
+    if (canvas.ds.scale > 0.2) {
+      // Limit min zoom
+      canvas.ds.scale *= 0.8;
+      canvas.setDirty(true, true);
+    }
+  };
+
   /**
    * Resizes the canvas to fit its container
    */
