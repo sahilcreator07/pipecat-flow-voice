513144b60f5858f654dc4d0bb9a8e68916b540bc
Paul Kompfner <paul@daily.co>
2025-05-23 17:01:20 -0400

    [WIP] More progress on “direct functions”: add tests for validation

--- tests/test_flows_direct_functions.py
+++ tests/test_flows_direct_functions.py
@@ -1,6 +1,7 @@
 import unittest
 from typing import Optional, Union
 
+from pipecat_flows.exceptions import InvalidFunctionError
 from pipecat_flows.manager import FlowManager
 from pipecat_flows.types import FlowsDirectFunction
 
@@ -16,8 +17,8 @@ class TestFlowsDirectFunction(unittest.TestCase):
     def test_name_is_set_from_function(self):
         """Test that FlowsDirectFunction extracts the name from the function."""
 
-        def my_function(flow_manager: FlowManager):
-            return {}
+        async def my_function(flow_manager: FlowManager):
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function)
         self.assertEqual(func.name, "my_function")
@@ -25,14 +26,14 @@ class TestFlowsDirectFunction(unittest.TestCase):
     def test_description_is_set_from_function(self):
         """Test that FlowsDirectFunction extracts the description from the function."""
 
-        def my_function_short_description(flow_manager: FlowManager):
+        async def my_function_short_description(flow_manager: FlowManager):
             """This is a test function."""
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_short_description)
         self.assertEqual(func.description, "This is a test function.")
 
-        def my_function_long_description(flow_manager: FlowManager):
+        async def my_function_long_description(flow_manager: FlowManager):
             """
             This is a test function.
 
@@ -40,7 +41,7 @@ class TestFlowsDirectFunction(unittest.TestCase):
 
             Trust me, you'll want to use it.
             """
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_long_description)
         self.assertEqual(
@@ -51,16 +52,16 @@ class TestFlowsDirectFunction(unittest.TestCase):
     def test_properties_are_set_from_function(self):
         """Test that FlowsDirectFunction extracts the properties from the function."""
 
-        def my_function_no_params(flow_manager: FlowManager):
-            return {}
+        async def my_function_no_params(flow_manager: FlowManager):
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_no_params)
         self.assertEqual(func.properties, {})
 
-        def my_function_simple_params(
+        async def my_function_simple_params(
             name: str, age: int, height: Union[float, None], flow_manager: FlowManager
         ):
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_simple_params)
         self.assertEqual(
@@ -72,13 +73,13 @@ class TestFlowsDirectFunction(unittest.TestCase):
             },
         )
 
-        def my_function_complex_params(
+        async def my_function_complex_params(
             address_lines: list[str],
             nickname: str | int | float,
             extra: Optional[dict[str, str]],
             flow_manager: FlowManager,
         ):
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_complex_params)
         self.assertEqual(
@@ -100,27 +101,27 @@ class TestFlowsDirectFunction(unittest.TestCase):
     def test_required_is_set_from_function(self):
         """Test that FlowsDirectFunction extracts the required properties from the function."""
 
-        def my_function_no_params(flow_manager: FlowManager):
-            return {}
+        async def my_function_no_params(flow_manager: FlowManager):
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_no_params)
         self.assertEqual(func.required, [])
 
-        def my_function_simple_params(
+        async def my_function_simple_params(
             name: str, age: int, height: Union[float, None] = None, flow_manager: FlowManager = None
         ):
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_simple_params)
         self.assertEqual(func.required, ["name", "age"])
 
-        def my_function_complex_params(
+        async def my_function_complex_params(
             address_lines: list[str],
             nickname: str | int | None = "Bud",
             extra: Optional[dict[str, str]] = None,
             flow_manager: FlowManager = None,
         ):
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function_complex_params)
         self.assertEqual(func.required, ["address_lines"])
@@ -128,7 +129,9 @@ class TestFlowsDirectFunction(unittest.TestCase):
     def test_property_descriptions_are_set_from_function(self):
         """Test that FlowsDirectFunction extracts the property descriptions from the function."""
 
-        def my_function(name: str, age: int, height: Union[float, None], flow_manager: FlowManager):
+        async def my_function(
+            name: str, age: int, height: Union[float, None], flow_manager: FlowManager
+        ):
             """
             This is a test function.
 
@@ -137,7 +140,7 @@ class TestFlowsDirectFunction(unittest.TestCase):
                 age (int): The age of the person.
                 height (float | None): The height of the person in meters. Defaults to None.
             """
-            return {}
+            return {}, None
 
         func = FlowsDirectFunction(function=my_function)
 
@@ -156,6 +159,21 @@ class TestFlowsDirectFunction(unittest.TestCase):
                 },
             },
         )
+
+    def test_invalid_functions_fail_validation(self):
+        """Test that invalid functions fail FlowsDirectFunction validation."""
+
+        def my_function_non_async(flow_manager: FlowManager):
+            return {}, None
+
+        with self.assertRaises(InvalidFunctionError):
+            FlowsDirectFunction.validate_function(my_function_non_async)
+
+        async def my_function_missing_flow_manager():
+            return {}, None
+
+        with self.assertRaises(InvalidFunctionError):
+            FlowsDirectFunction.validate_function(my_function_missing_flow_manager)
 
 
 if __name__ == "__main__":
